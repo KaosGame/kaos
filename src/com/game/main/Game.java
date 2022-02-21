0d463ae4641db058edc8e1f54830483eb2cba31c
aixoio <aixoio@yahoo.com>
2022-02-21 19:11:34 +1100

    The start!

---
+++ src/com/game/main/Game.java
@@ -0,0 +1,49 @@
+package com.game.main;
+
+import javax.swing.JFrame;
+
+import com.game.display.components.GamePanel;
+
+public class Game {
+	
+	private JFrame frame;
+	private GamePanel gamePanel;
+	
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = 540;
+	
+	public Game(String title) {
+		
+		this.frame = new JFrame();
+		this.gamePanel = new GamePanel();
+		
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setLocationRelativeTo(null);
+		this.frame.setSize(Game.WIDTH, Game.HEIGHT);
+		this.frame.setTitle(title);
+		this.frame.setResizable(false);
+		this.frame.setLayout(null);
+		
+		
+		this.gamePanel.setBounds(0, 0, Game.WIDTH, Game.HEIGHT);
+		this.frame.add(this.gamePanel);
+		
+		
+		
+		this.frame.setVisible(true);
+		
+		this.gamePanel.startGameLoop();
+		
+	}
+	
+	public static int clamp(int var, int max, int min) {
+		if (var >= max) {
+			return max;
+		} else if (var <= min) {
+			return min;
+		} else {
+			return var;
+		}
+	}
+
+}
---
+++ src/com/game/main/Main.java
@@ -0,0 +1,11 @@
+package com.game.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		new Game("Kaos");
+
+	}
+
+}
