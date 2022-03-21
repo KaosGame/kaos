43c7ca7aebf3dfb85e74cc78e590e6bb6b54982a
aixoio <aixoio@yahoo.com>
2022-03-21 16:28:18 +1100

    Add score to death

--- src/com/game/entities/base/EntityDeathMessages.java
+++ src/com/game/entities/base/EntityDeathMessages.java
@@ -1,9 +1,11 @@
 package com.game.entities.base;
 
+import com.game.main.Game;
+
 public enum EntityDeathMessages {
 	
-	STARVING("You starved to death!"),
-	COMMAND("You have died to a command!");
+	STARVING("You starved to death with the score of %d!"),
+	COMMAND("You have died to a command with the score of %d!");
 	
 	
 	private final String MESSAGE;
@@ -15,6 +17,16 @@ public enum EntityDeathMessages {
 	}
 	
 	public String getDeathMessage() {
+		
+		long deathScore = Game.PLAYER.getCoins();
+		
+		String text = String.format(this.MESSAGE, deathScore);
+		
+		return text;
+		
+	}
+	
+	public String getTrueText() {
 		
 		return this.MESSAGE;
 		
