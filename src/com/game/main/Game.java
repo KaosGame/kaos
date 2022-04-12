4a1f95a6034fa241e21526d361dcdf191d52590d
aixoio <aixoio@yahoo.com>
2022-04-12 20:01:36 +1000

    Remove import

--- src/com/game/main/Game.java
+++ src/com/game/main/Game.java
@@ -2,7 +2,6 @@ package com.game.main;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
